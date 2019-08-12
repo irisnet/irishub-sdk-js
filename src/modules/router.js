ad1e1c4c309431912ce4e27251133d59787d16c7
zhangzhiqiang <745124335@qq.com>
2019-08-12 19:19:32 +0800

    refactor code

--- src/modules/router.js
+++ src/modules/router.js
@@ -1,10 +1,9 @@
 import {IrisRouter} from "./router-iris"
 import {CosmosRouter} from "./router-cosmos"
 
-export class ApiRouter {
-    static getSubRouter(chain){
-        return ApiRouter[chain]
-    }
-}
+export const ApiRouter = {};
+ApiRouter.getSubRouter = function(chain){
+    return ApiRouter[chain]
+};
 ApiRouter.iris = IrisRouter;
 ApiRouter.cosmos = CosmosRouter;
