d6121e81a1cf78161b9aa6138bea21bcd177c3a0
zhangyelong <yelong@bianjie.ai>
2020-02-12 21:44:34 +0800

    Add distr txs

--- src/types/distribution.ts
+++ src/types/distribution.ts
@@ -1,4 +1,80 @@
-import { Coin } from './types';
+import { Coin, Msg } from './types';
+
+export class MsgSetWithdrawAddress implements Msg {
+  type: string;
+  value: {
+    delegator_addr: string;
+    withdraw_addr: string;
+  };
+
+  constructor(delegator_addr: string, withdraw_addr: string) {
+    this.type = 'irishub/distr/MsgModifyWithdrawAddress';
+    this.value = {
+      delegator_addr: delegator_addr,
+      withdraw_addr: withdraw_addr,
+    };
+  }
+
+  getSignBytes(): object {
+    return this.value;
+  }
+}
+
+export class MsgWithdrawDelegatorRewardsAll implements Msg {
+  type: string;
+  value: {
+    delegator_addr: string;
+  };
+
+  constructor(delegator_addr: string) {
+    this.type = 'irishub/distr/MsgWithdrawDelegationRewardsAll';
+    this.value = {
+      delegator_addr: delegator_addr,
+    };
+  }
+
+  getSignBytes(): object {
+    return this.value;
+  }
+}
+
+export class MsgWithdrawDelegatorReward implements Msg {
+  type: string;
+  value: {
+    delegator_addr: string;
+    withdraw_addr: string;
+  };
+
+  constructor(delegator_addr: string, withdraw_addr: string) {
+    this.type = 'irishub/distr/MsgWithdrawDelegationReward';
+    this.value = {
+      delegator_addr: delegator_addr,
+      withdraw_addr: withdraw_addr,
+    };
+  }
+
+  getSignBytes(): object {
+    return this.value;
+  }
+}
+
+export class MsgWithdrawValidatorRewardsAll implements Msg {
+  type: string;
+  value: {
+    validator_addr: string;
+  };
+
+  constructor(validator_addr: string) {
+    this.type = 'irishub/distr/MsgWithdrawValidatorRewardsAll';
+    this.value = {
+      validator_addr: validator_addr,
+    };
+  }
+
+  getSignBytes(): object {
+    return this.value;
+  }
+}
 
 export interface Rewards {
   total: Coin[];
