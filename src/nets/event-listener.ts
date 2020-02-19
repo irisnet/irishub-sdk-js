7ceae2ac6f56cf5b77f265ab111ab55cc4dcdc36
zhangyelong <yelong@bianjie.ai>
2020-02-19 17:53:47 +0800

    Add subscribeNewBlock

--- src/nets/event-listener.ts
+++ src/nets/event-listener.ts
@@ -3,27 +3,26 @@ import { base64ToBytes, bytesToBase64 } from '@tendermint/belt';
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import Utils from '../utils/utils';
 import SdkError from '../errors';
+import { Client } from '../client';
 import * as is from 'is_js';
 import * as types from '../types';
 import * as EventEmitter from 'events';
 import { obj as Pumpify } from 'pumpify';
 import * as Ndjson from 'ndjson';
 import * as Websocket from 'websocket-stream';
+import { Interface } from 'readline';
 
 /**
  * IRISHub Event Listener
  */
-export default class EventListener {
-  private url: string;
+export class EventListener {
+  /** @hidden */
+  private client: Client;
   private ws: any;
   private em: EventEmitter;
 
-  /**
-   * @param url IRISHub RPC Address
-   */
-  constructor(url: string) {
-    // TODO: wss support
-    this.url = url + '/websocket';
+  constructor(client: Client) {
+    this.client = client;
     this.em = new EventEmitter();
   }
 
@@ -31,7 +30,10 @@ export default class EventListener {
    * Initialize ws client
    */
   connect(): void {
-    this.ws = Pumpify(Ndjson.stringify(), Websocket(this.url));
+    this.ws = Pumpify(
+      Ndjson.stringify(),
+      Websocket(this.client.config.node + '/websocket')
+    );
     this.ws.on('data', (resp: string) => {
       const data = JSON.parse(resp);
       console.log(JSON.stringify(data));
@@ -44,34 +46,127 @@ export default class EventListener {
     this.ws.on('error', (err: string) => {
       this.em.emit('error', err);
     });
-
-    // TODO: removing, only used for testing
-    this.ws.write({
-      jsonrpc: '2.0',
-      method: 'subscribe',
-      id: 0,
-      params: {
-        query: "tm.event='NewBlock'",
-      },
-    });
-    // TODO: removing, only used for testing
-    this.onError(function(error: SdkError) {});
   }
 
   /**
    * Disconnect from server
    */
   disconnect(): void {
-    this.ws.destroy();
+    // Unsubscribe all from server
+    this.ws.write({
+      jsonrpc: '2.0',
+      method: 'unsubscribe_all',
+      id: 'unsubscribe_all',
+    });
+
+    this.em.on('unsubscribe_all#event', (error, data) => {
+      console.log(error);
+      console.log(data);
+
+      // Destroy ws instance
+      this.ws.destroy();
+      // Remove all listeners
+      this.em.removeAllListeners();
+    });
   }
 
   /**
    * Listen on error
    * @param callback Callback function with a [[SdkError]] param
    */
-  onError(callback: (error: SdkError) => any): void {
+  onError(callback: (error: SdkError) => void): void {
     this.em.on('error', (err: string) => {
       callback(new SdkError(err));
     });
   }
+
+  unscribe(event: EventSubscription): void {
+    // Unsubscribe all from server
+    this.ws.write({
+      jsonrpc: '2.0',
+      method: 'unsubscribe',
+      id: 'unsubscribe#' + event.id,
+      params: {
+        query: event.query,
+      },
+    });
+
+    this.em.on('unsubscribe#' + event.id, (error, data) => {
+      console.log(error);
+      console.log(data);
+      // Remove listeners
+      this.em.removeAllListeners(event.id + '#event');
+      this.em.removeAllListeners('unsubscribe#' + event.id + '#event');
+    });
+  }
+
+  /**
+   * Subscribe NewBlock
+   * @param callback A function to receive notifications
+   * @returns
+   */
+  subscribeNewBlock(
+    callback: (error?: SdkError, block?: types.EventDataNewBlock) => void
+  ): EventSubscription {
+    // Build and send subscription
+    const eventType = 'NewBlock';
+    const id = eventType + Math.random().toString(16);
+    const query = new EventQueryBuilder()
+      .addCondition(Event.Type, eventType)
+      .build();
+
+    this.ws.write({
+      jsonrpc: '2.0',
+      method: 'subscribe',
+      id: { id },
+      params: {
+        query: { query },
+      },
+    });
+
+    // Listen for new blocks, decode and callback
+    this.em.on(id + '#event', (error, data) => {
+      if (error) {
+        callback(
+          new SdkError(error.message, error.code, error.data),
+          undefined
+        );
+      }
+      const eventBlock: types.EventDataNewBlock = data.data.value;
+      callback(undefined, data);
+    });
+
+    // Return an EventSubscription instance, so client could use to unsubscribe this context
+    return { id, query, callback };
+  }
+}
+
+export interface EventSubscription {
+  id: string;
+  query: string;
+  callback: (error?: SdkError, block?: types.EventDataNewBlock) => void;
+}
+
+export class EventQueryBuilder {
+  private condition: Array<{ K: string; V: string }> = [];
+  addCondition(event: Event, value: string | EventAction): EventQueryBuilder {
+    this.condition.push({ K: event, V: "'" + value + "'" });
+    return this;
+  }
+  build(): string {
+    return this.condition.join('&');
+  }
+}
+
+export enum Event {
+  Type = 'tm.event',
+  Action = 'action',
+  Sender = 'sender',
+  Recipient = 'recipient',
+}
+
+export enum EventAction {
+  Send = 'send',
+  Burn = 'burn',
+  SetMemoRegexp = 'set-memo-regexp',
 }
---
+++ src/types/events.ts
@@ -0,0 +1,94 @@
+export interface EventDataNewBlock {
+  block: EventDataBlock;
+  result_begin_block: EventDataResultBeginBlock;
+  result_end_block: EventDataResultEndBlock;
+}
+
+export interface EventDataBlock {
+  header: EventDataHeader;
+  data: EventDataBlockData;
+  evidence: EventDataEvidence[];
+  last_commit: EventDataLastCommit;
+}
+
+export interface EventDataBlockData {
+  txs: string[];
+}
+
+export interface EventDataEvidence {
+  // TODO
+}
+
+export interface EventDataHeader {
+  version: EventDataVersion;
+  chain_id: string;
+  height: string;
+  time: string;
+  num_txs: string;
+  total_txs: string;
+  last_block_id: EventDataBlockID;
+  last_commit_hash: string;
+  data_hash: string;
+  validators_hash: string;
+  next_validators_hash: string;
+  consensus_hash: string;
+  app_hash: string;
+  last_results_hash: string;
+  evidence_hash: string;
+  proposer_address: string;
+}
+
+export interface EventDataBlockID {
+  hash: string;
+  parts: EventDataParts;
+}
+
+export interface EventDataParts {
+  total: string;
+  hash: string;
+}
+
+export interface EventDataVersion {
+  block: string;
+  app: string;
+}
+
+export interface EventDataLastCommit {
+  block_id: EventDataBlockID;
+  precommits: EventDataPrecommit[];
+}
+
+export interface EventDataPrecommit {
+  type: number;
+  height: string;
+  round: string;
+  block_id: EventDataBlockID;
+  timestamp: string;
+  validator_address: string;
+  validator_index: string;
+  signature: string;
+}
+
+export interface EventDataResultBeginBlock {
+  tags: EventDataTag[];
+}
+
+export interface EventDataTag {
+  key: string;
+  value: string;
+}
+
+export interface EventDataResultEndBlock {
+  validator_updates: EventDataValidatorUpdate[];
+  tags: EventDataTag[];
+}
+
+export interface EventDataValidatorUpdate {
+  pub_key: EventDataPubKey;
+  power: number;
+}
+
+export interface EventDataPubKey {
+  type: string;
+  data: string;
+}
